916cd50f16b5aa589744910b983266e4302b30c1
root <gmerit2030@gmail.com>
2023-07-26 05:51:15 +0000

    promise

---
+++ 0x01-ES6_promise/createUser.js
@@ -0,0 +1,11 @@
+function createUser() {
+  // Your code to create a user goes here
+  // Assuming the user's first name is 'Guillaume' and last name is 'Salva'
+  return {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  };
+}
+
+module.exports = createUser;
+
---
+++ 0x01-ES6_promise/uploadPhoto.js
@@ -0,0 +1,11 @@
+function uploadPhoto() {
+  // Your code to upload the photo goes here
+  // Assuming the photo profile is 'photo-profile-1'
+  return {
+    status: 200,
+    body: 'photo-profile-1',
+  };
+}
+
+module.exports = uploadPhoto;
+
